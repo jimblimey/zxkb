ef1884aac08490e76c5b8ee4f6f7b02f98b08c63
Jim Blimey <jimblimey@outlook.com>
2021-03-12 15:21:13 +0000

    Added remaining uinput bits

--- zxkb.c
+++ zxkb.c
@@ -21,6 +21,17 @@ const char* keys[8][5] = {
 	{"SPACE","LEFTCTRL","M","N","B"}
 }; 
 
+const int vkeys[8][5] = {
+	{KEY_1,KEY_2,KEY_3,KEY_4,KEY_5},
+	{KEY_Q,KEY_W,KEY_E,KEY_R,KEY_T},
+	{KEY_A,KEY_S,KEY_D,KEY_F,KEY_G},
+	{KEY_0,KEY_9,KEY_8,KEY_7,KEY_6},
+	{KEY_P,KEY_O,KEY_I,KEY_U,KEY_Y},
+	{KEY_LEFTSHIFT,KEY_Z,KEY_X,KEY_C,KEY_V},
+	{KEY_ENTER,KEY_L,KEY_K,KEY_J,KEY_H},
+	{KEY_SPACE,KEY_LEFTCTRL,KEY_M,KEY_N,KEY_B}
+};
+
 int keytrack[8][5] = {
 	{0,0,0,0,0},
 	{0,0,0,0,0},
@@ -62,14 +73,17 @@ int main(void) {
 	struct uinput_setup usetup;
 	int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 	ioctl(fd, UI_SET_EVBIT, EV_KEY);
-	ioctl(fd, UI_SET_KEYBIT, KEY_SPACE);
-	ioctl(fd, UI_SET_KEYBIT, KEY_Q);
-	ioctl(fd, UI_SET_KEYBIT, KEY_W);
-	// TODO add rest of keys
+
+	for(int i=0;i<8;i++) {
+		for(int j=0;j<5;j++) {
+			ioctl(fd, UI_SET_KEYBIT, vkeys[i][j]);
+		}
+	}
+
 	memset(&usetup, 0, sizeof(usetup));
 	usetup.id.bustype = BUS_USB;
 	usetup.id.vendor = 0xB00B;
-	usetup.id.product = 0xA453;
+	usetup.id.product = 0xA45E;
 	strcpy(usetup.name, "ZXKB");
 	ioctl(fd, UI_DEV_SETUP, &usetup);
 	ioctl(fd, UI_DEV_CREATE);
@@ -81,13 +95,19 @@ int main(void) {
 			digitalWrite(addresslines[i], 0);
 			for(int j=0;j<5;j++) {
 				isFree = digitalRead(datalines[j]);
+				// Key down
 				if(isFree == 0 && keytrack[i][j] == 0) {
 					printf("Pressed %s\n",keys[i][j]);
 					keytrack[i][j] = 1;
+					sendkeypress(fd, EV_KEY, vkeys[i][j], 1);
+					sendkeypress(fd, EV_SYN, SYN_REPORT, 0);
 				}
+				// Key up
 				else if(isFree == 1 && keytrack[i][j] == 1) {
 					printf("Released %s\n",keys[i][j]);
 					keytrack[i][j] = 0;
+					sendkeypress(fd, EV_KEY, vkeys[i][j], 0);
+					sendkeypress(fd, EV_SYN, SYN_REPORT, 0);
 				}
 			}
 			digitalWrite(addresslines[i], 1);
